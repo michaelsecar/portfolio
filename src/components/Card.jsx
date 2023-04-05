03a09e25506df1010284d8ca5218bd9f0cfe65b6
FanFaRoNoObSkY <michaelsecar@gmail.com>
2023-04-04 19:12:00 -0500

    Add: Style to transparent elements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,21 @@
+import "./Card.css"
+import { ArrowUp, GitHub } from "../components/Icons"
 
-import './Card.css'
-
-const Card = ({title, description, image, alt="undefined"}) => {
-    return (
-        <div className="card-container">
-            <h3 className="card-title">
-                {title}
-            </h3>
-            <div className="card-description">
-                {description} 
-            </div>
-            <img className="card-image" src={image} alt={alt} />
+const Card = ({ title, description, image, alt = "undefined" }) => {
+  return (
+    <div className="card-container">
+      <div className="card-header">
+        <h3>{title}</h3>
+        <div className="card-source">
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <GitHub />
+          </a>
         </div>
-    )
+      </div>
+      <div className="card-description">{description}</div>
+      <img className="card-image" src={image} alt={alt} />
+    </div>
+  )
 }
 
 export default Card
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,24 +1,41 @@
-import Card from '../components/Card'
-import './Projects.css'
+import Card from "../components/Card"
+import "./Projects.css"
 
 const Projects = () => {
-    return (
-        <div className="container projects-container">
-            <h1 className="title">
-                <span className="green-text h1">Proyectos</span>  en los que he trabajado
-            </h1>
-            <div className="description">
-                <p className="description-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi obcaecati hic doloribus, accusantium dolor aspernatur? Culpa dicta distinctio perspiciatis sequi.
-                </p>
-            </div>
-            <div className="project-cards-container">
-                <Card title="Alguno" description="Esta es la descripcion de ejemplo" image="moon.jpg" alt="Texto alternativo"/>
-                <Card title="Alguno" description="Esta es la descripcion de ejemplo" image="moon.jpg" alt="Texto alternativo"/>
-                <Card title="Alguno" description="Esta es la descripcion de ejemplo" image="moon.jpg" alt="Texto alternativo"/>
-            </div>
-       </div>
-    )
+  return (
+    <div className="container projects-container">
+      <h1 className="title">
+        <span className="green-text h1">Proyectos</span> en los que he trabajado
+      </h1>
+      <div className="description">
+        <p className="description-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+          obcaecati hic doloribus, accusantium dolor aspernatur? Culpa dicta
+          distinctio perspiciatis sequi.
+        </p>
+      </div>
+      <div className="project-cards-container">
+        <Card
+          title="Project 1"
+          description="Descripcion del primer proyecto"
+          image="moon.jpg"
+          alt="Texto alternativo"
+        />
+        <Card
+          title="Project 2"
+          description="Descripcion del segundo proyecto"
+          image="moon.jpg"
+          alt="Texto alternativo"
+        />
+        <Card
+          title="Project 3"
+          description="Descripcion del tercer proyecto"
+          image="moon.jpg"
+          alt="Texto alternativo"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Projects
