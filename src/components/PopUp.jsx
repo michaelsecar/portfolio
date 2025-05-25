23bfff2d6240d5aea028d1985a96c613fca86ee9
Michael Secarlos AC <michaelsecar@gmail.com>
2025-05-24 22:00:46 -0500

    Fix modal display

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,14 +1,36 @@
 import { motion, AnimatePresence } from "framer-motion";
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
 
 export const PopUp = ({ children, isOpen, onClose }) => {
-  return (
+  useEffect(() => {
+    const rootElement = document.querySelector("#root");
+    if (isOpen && rootElement) {
+      rootElement.classList.add("overflow-hidden");
+    } else {
+      rootElement?.classList.remove("overflow-hidden");
+    }
+    return () => {
+      rootElement?.classList.remove("overflow-hidden");
+    };
+  }, [isOpen]);
+
+  return createPortal(
     <AnimatePresence>
       {isOpen && (
         <motion.div
           animate={{ opacity: 1 }}
-          initial={{ opacity: 0 }}
+          initial={{
+            opacity: 0,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,0.8)",
+            zIndex: 10,
+          }}
           exit={{ opacity: 0 }}
-          className="absolute top-0 left-0 w-full h-full bg-opacity-80 bg-dark"
           onClick={onClose}
         >
           <div className="flex items-center justify-center h-full">
@@ -21,6 +43,7 @@ export const PopUp = ({ children, isOpen, onClose }) => {
           </div>
         </motion.div>
       )}
-    </AnimatePresence>
+    </AnimatePresence>,
+    document.body
   );
 };
